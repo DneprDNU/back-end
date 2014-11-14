002f45d36f1a0d24971fa410d9ea9bf0efaa8973
DemYura <demyura@gmail.com>
2014-11-14 23:58:15 +0100

    Added mock data

--- src/main/java/org/dnu/filestorage/controller/generic/GenericController.java
+++ src/main/java/org/dnu/filestorage/controller/generic/GenericController.java
@@ -8,7 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
-import javax.transaction.Transactional;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,7 +16,6 @@ import java.util.Map;
  * @author demyura
  * @since 14.11.14
  */
-@Transactional
 public abstract class GenericController<T extends NamedEntity> {
     private Logger logger = LoggerFactory.getLogger(GenericController.class);
 
