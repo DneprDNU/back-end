36be7a4e45d0faa98c9472cef8434a5f60d72e3d
Yaroslav <yaroslav@Yaroslavs-MacBook-Pro.local>
2014-11-23 19:42:57 +0200

    Adding FileUploader class

---
+++ src/main/java/org/dnu/filestorage/utils/FileUploader.java
@@ -0,0 +1,34 @@
+package org.dnu.filestorage.utils;
+
+
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.*;
+
+public class FileUploader {
+
+    public static final String FOLDER = "/resources/";
+
+    public static String uploadFile(HttpServletRequest request, MultipartFile multipartFile) throws IOException {
+        File f = new File(request.getRealPath("") + FOLDER + multipartFile.getOriginalFilename());
+
+        int i = 0;
+        while (f.exists()) {
+            i++;
+            f = new File(request.getRealPath("") + FOLDER + i + multipartFile.getOriginalFilename());
+        }
+
+        byte[] bytes = multipartFile.getBytes();
+        BufferedOutputStream stream =
+                new BufferedOutputStream(new FileOutputStream(f));
+        stream.write(bytes);
+        stream.close();
+
+        if (i != 0) {
+            return FOLDER + i + multipartFile.getOriginalFilename();
+        } else {
+            return FOLDER + multipartFile.getOriginalFilename();
+        }
+    }
+}
