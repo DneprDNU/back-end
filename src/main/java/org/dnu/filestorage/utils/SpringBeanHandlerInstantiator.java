ea23682dc33ec8223204b4d39b36df4c2d214de9
Yaroslav Kharchenko <undertext.2010@gmail.com>
2015-01-18 15:45:51 +0200

    Injections in custom jackson deserializer

---
+++ src/main/java/org/dnu/filestorage/utils/SpringBeanHandlerInstantiator.java
@@ -0,0 +1,64 @@
+package org.dnu.filestorage.utils;
+
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.cfg.HandlerInstantiator;
+import com.fasterxml.jackson.databind.cfg.MapperConfig;
+import com.fasterxml.jackson.databind.introspect.Annotated;
+import com.fasterxml.jackson.databind.jsontype.TypeIdResolver;
+import com.fasterxml.jackson.databind.jsontype.TypeResolverBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class SpringBeanHandlerInstantiator extends HandlerInstantiator {
+
+
+    private ApplicationContext applicationContext;
+
+    @Autowired
+    public SpringBeanHandlerInstantiator(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    @Override
+    public JsonDeserializer<?> deserializerInstance(DeserializationConfig config,
+                                                    Annotated annotated,
+                                                    Class<?> deserClass) {
+        try {
+            return (JsonDeserializer<?>) applicationContext.getBean(deserClass);
+        } catch (Exception e) {
+            // Return null and let the default behavior happen
+        }
+        return null;
+    }
+
+    @Override
+    public KeyDeserializer keyDeserializerInstance(DeserializationConfig config,
+                                                   Annotated annotated,
+                                                   Class<?> keyDeserClass) {
+        try {
+            return (KeyDeserializer) applicationContext.getBean(keyDeserClass);
+        } catch (Exception e) {
+            // Return null and let the default behavior happen
+        }
+        return null;
+    }
+
+    @Override
+    public JsonSerializer<?> serializerInstance(SerializationConfig config, Annotated annotated, Class<?> serClass) {
+        return null;
+    }
+
+    @Override
+    public TypeResolverBuilder<?> typeResolverBuilderInstance(MapperConfig<?> config, Annotated annotated, Class<?> builderClass) {
+        return null;
+    }
+
+    @Override
+    public TypeIdResolver typeIdResolverInstance(MapperConfig<?> config, Annotated annotated, Class<?> resolverClass) {
+        return null;
+    }
+
+}
