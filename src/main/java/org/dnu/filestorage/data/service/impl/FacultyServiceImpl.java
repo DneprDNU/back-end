2743d0bbdaef15c1cff85eda627c9da01e4213b5
Yaroslav Kharchenko <undertext.2010@gmail.com>
2015-01-22 00:35:53 +0200

    Faculty Service

--- src/main/java/org/dnu/filestorage/data/service/impl/FacultyServiceImpl.java
+++ src/main/java/org/dnu/filestorage/data/service/impl/FacultyServiceImpl.java
@@ -1,12 +1,18 @@
 package org.dnu.filestorage.data.service.impl;
 
+import org.dnu.filestorage.data.dao.DepartmentDAO;
 import org.dnu.filestorage.data.dao.FacultyDAO;
+import org.dnu.filestorage.data.model.Department;
 import org.dnu.filestorage.data.model.Faculty;
+import org.dnu.filestorage.data.model.Speciality;
+import org.dnu.filestorage.data.model.Subject;
 import org.dnu.filestorage.data.service.FacultyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author demyura
@@ -17,6 +23,9 @@ import javax.transaction.Transactional;
 public class FacultyServiceImpl extends GenericServiceImpl<FacultyDAO, Faculty> implements FacultyService {
 
     @Autowired
+    private DepartmentDAO departmentDAO;
+
+    @Autowired
     public FacultyServiceImpl(FacultyDAO dao) {
         super(dao);
     }
@@ -24,5 +33,26 @@ public class FacultyServiceImpl extends GenericServiceImpl<FacultyDAO, Faculty> 
     @Override
     public Faculty get(Long id) {
         return dao.getFacultyWithRelations(id);
+    }
+
+    @Override
+    public Faculty update(Faculty entity) {
+        List<Department> departmentList = new ArrayList<Department>();
+        for (Department department: entity.getDepartments()){
+            Long departmentId = department.getId();
+            Department departmentFromDatabase = departmentDAO.get(departmentId);
+            departmentList.add(departmentFromDatabase);
+        }
+
+        entity.setDepartments(departmentList);
+        return dao.update(entity);
+
+    }
+
+    @Override
+    public Faculty create(Faculty entity) {
+        Faculty faculty = dao.create(new Faculty());
+        faculty.setId(entity.getId());
+        return update(faculty);
     }
 }
