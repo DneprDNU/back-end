0a3cffac7e150bec511a58178520a13ac61f753c
Yaroslav Kharchenko <undertext.2010@gmail.com>
2015-02-13 01:34:13 +0200

    Generic controller change for departments

--- src/main/java/org/dnu/filestorage/controller/DepartmentController.java
+++ src/main/java/org/dnu/filestorage/controller/DepartmentController.java
@@ -2,6 +2,7 @@ package org.dnu.filestorage.controller;
 
 import com.wordnik.swagger.annotations.Api;
 import org.dnu.filestorage.controller.generic.GenericController;
+import org.dnu.filestorage.controller.generic.GenericImageController;
 import org.dnu.filestorage.data.model.Department;
 import org.dnu.filestorage.data.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +16,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @Api(value = "departments", description = "Endpoint for department management")
 @RequestMapping("/rest/department")
-public class DepartmentController extends GenericController<DepartmentService, Department> {
+public class DepartmentController extends GenericImageController<DepartmentService, Department> {
 
     @Autowired
     public DepartmentController(DepartmentService service) {
-        super(service);
+        super(service, Department.class);
     }
 }
