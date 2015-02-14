e24b27744398aa2ab7a582508a091917ab6e87f2
Yaroslav Kharchenko <undertext.2010@gmail.com>
2015-02-14 22:37:46 +0200

    Images support for teachers controller

--- src/main/java/org/dnu/filestorage/controller/TeacherController.java
+++ src/main/java/org/dnu/filestorage/controller/TeacherController.java
@@ -2,6 +2,7 @@ package org.dnu.filestorage.controller;
 
 import com.wordnik.swagger.annotations.Api;
 import org.dnu.filestorage.controller.generic.GenericController;
+import org.dnu.filestorage.controller.generic.GenericImageController;
 import org.dnu.filestorage.data.model.Teacher;
 import org.dnu.filestorage.data.service.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +16,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @Api(value = "teachers", description = "Endpoint for teacher management")
 @RequestMapping("/rest/teacher")
-public class TeacherController extends GenericController<TeacherService, Teacher> {
+public class TeacherController extends GenericImageController<TeacherService, Teacher> {
 
     @Autowired
     public TeacherController(TeacherService service) {
-        super(service);
+        super(service, Teacher.class);
     }
 }
