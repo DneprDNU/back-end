a36c557544cb3e1a3573b12bac25dccfc6fd72f4
DemYura <demyura@gmail.com>
2015-04-14 01:01:10 +0200

    minor fix

--- src/main/java/org/dnu/filestorage/data/model/Resource.java
+++ src/main/java/org/dnu/filestorage/data/model/Resource.java
@@ -19,7 +19,7 @@ import java.util.List;
 public class Resource extends NamedEntity {
     @ManyToMany(mappedBy = "resources")
     private List<Category> categories = new LinkedList<Category>();
-    @ManyToMany(fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "resources")
     private List<Subject> subjects = new LinkedList<Subject>();
 
     @ManyToOne()
