e149fe92f1d687f52f20c61a0d67024e9bbb4939
DemYura <demyura@gmail.com>
2015-05-24 23:08:44 +0200

    fixed users list query

--- src/main/java/org/dnu/filestorage/data/dao/impl/UserDAOImpl.java
+++ src/main/java/org/dnu/filestorage/data/dao/impl/UserDAOImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
@@ -40,5 +41,15 @@ public class UserDAOImpl extends GenericDAOImpl<User> implements UserDAO {
         entity.setPassword(passwordEncoder.encodePassword(entity.getPassword(), null));
         entityManager.persist(entity);
         return entity;
+    }
+
+    @Override
+    public List<User> list(int from, int to) {
+        TypedQuery<User> query = entityManager.createQuery("select a from User a " +
+                        "order by a.username asc",
+                entityClass);
+        query.setFirstResult(from);
+        query.setMaxResults(to - from);
+        return query.getResultList();
     }
 }
